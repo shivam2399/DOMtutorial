120131fd25a12bb37564514e5f985a079c8ab654
shivam2399 <dubey.shivam2399@gmail.com>
2022-11-14 11:27:55 +0530

    first commit

---
+++ dom.js
@@ -0,0 +1,10 @@
+let headerTitle = document.getElementById('header-title');
+let header = document.getElementById('main-header');
+
+header.style.borderBottom = 'solid 3px #000';
+
+var items = document.getElementsByClassName('title');
+
+items[0].style.fontWeight = 'bold';
+items[0].style.color = 'green';
+
