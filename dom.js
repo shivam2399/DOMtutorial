ed9c763d8a5fe2522f8a536ba165c686491dec69
shivam2399 <dubey.shivam2399@gmail.com>
2022-11-14 11:43:04 +0530

    changes in list items

--- dom.js
+++ dom.js
@@ -8,3 +8,10 @@ var items = document.getElementsByClassName('title');
 items[0].style.fontWeight = 'bold';
 items[0].style.color = 'green';
 
+
+var listItems = document.getElementsByClassName('list-group-item');
+ listItems[0].style.fontWeight = 'bold';
+ listItems[1].style.fontWeight = 'bold';
+ listItems[2].style.fontWeight = 'bold';
+ listItems[2].style.backgroundColor = 'green';
+ listItems[3].style.fontWeight = 'bold';
