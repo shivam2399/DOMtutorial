49e08e0ba974ab86278986bfcbeaa5d83351825b
shivam2399 <dubey.shivam2399@gmail.com>
2022-11-15 10:14:48 +0530

    using queryselector

--- dom.js
+++ dom.js
@@ -16,10 +16,10 @@ for(let i=0;i<li.length;i++) {
         li[i].style.fontWeight = 'bold';
      }
 
-let secondItem = document.querySelector('.list-group-item:nth-child(2)');
+     let secondItem = document.querySelector('.list-group-item:nth-child(2)');
 
-secondItem.style.backgroundColor = 'green';
-
-let thirdItem = document.querySelector('.list-group-item:nth-child(3)');
-
-thirdItem.style.visibility = 'hidden';
+     secondItem.style.backgroundColor = 'green';
+     
+     let thirdItem = document.querySelector('.list-group-item:nth-child(3)');
+     
+     thirdItem.style.visibility = 'hidden';
